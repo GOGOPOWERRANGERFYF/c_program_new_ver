f6851a70828de1d3ba569d0f66b1db14727e797c
GOGOPOWERRANGERFYF <392138779@qq.com>
2022-07-23 16:19:04 +0800

    modify and add

---
+++ sql/sql.py
@@ -0,0 +1,14 @@
+import imp
+
+
+'''
+    SQL structure query language 结构化查询语言
+    database 数据库
+    talbe 表
+    column 列
+    row 行
+
+    ALTER (修改)
+    
+    UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值 (升级)
+'''
