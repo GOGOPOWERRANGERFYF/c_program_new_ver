c3392b90c1cbf7124ca1d23a0705d04c7c9efd3a
GOGOPOWERRANGERFYF <392138779@qq.com>
2024-06-02 01:19:28 +0800

    modified

--- temp1.c
+++ temp1.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-typedef struct st2 st2;
-// 结构体自引用和不完整类型引用方法:通过指针
-struct st1 {
+typedef struct st2 st2; //结构体自引用和不完整类型引用方法:通过指针
+
+typedef struct st1 {
     unsigned long st1_id;
     st2 * st1_st2_pointer;
+} st1;
+
+typedef struct  st2 {
+
 };
 
-
 int main(void) {
+    printf("源神启动！！！\n");
+    st1 st1_instance;
     return 0;
 }
